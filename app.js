10a83a5040a6b45066e7fed92a4095489a5ac2f4
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-05 15:23:11 +0000

    problemas ft22 repaso loops

---
+++ app.js
@@ -0,0 +1,23 @@
+//Crea un programa que genere la tabla de multiplicar de un número ingresado por el usuario.
+// antes de ejecutar, instalar    npm install prompt-sync     en terminal
+const prompt = require('prompt-sync')();
+
+function tabla(num) {
+    console.log(`Tabla de multiplicar del ${num}`);
+    for (let i = 0; i <= 10; i++) {
+        console.log(`${num} * ${i} = ${num * i}`);
+    }
+}
+
+let numero = parseInt(prompt('Dame un valor mayor a 0: '));
+tabla(numero);
+
+//Escribe un bucle que calcule el factorial de un número dado.
+function factorialNum(num) {
+    factSum = num;
+    for (let i = num - 1; i >= 1; i--) {
+        factSum = factSum * i;      //5*4*3*2*1
+    }
+    return factSum;
+}
+console.log(factorialNum(parseInt(prompt('Dame un valor para calcular su factorial: '))));
